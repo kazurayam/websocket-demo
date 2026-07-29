6cc1eff3e125d1baf5b6eb92761c3a219c4988ab
kazurayam <kazuaki.urayama@gmail.com>
2026-07-29 22:02:02 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# websocket-demo
+
+I will transcript the articles by ROBERTO BUTTI in Dec 2024:
+
+1. [WebSocket with JavaScript and Bun](https://dev.to/robertobutti/websocket-with-javascript-and-bun-4o7c)
+2. [WebSocket Client with JavaScript](https://dev.to/robertobutti/websocket-client-with-javascript-54ec)
+3. [WebSocket broadcasting with JavaScript and Bun](https://dev.to/robertobutti/websocket-broadcasting-with-javascript-and-bun-3mkf)
+
+## How to run the demo
+
+Start the server:
+
+```
+$ cd $ROOT
+$ bun index.ts
+```
+
+You want to open a browser and navigate to `localhost:8080`.
+
+
+
---
+++ index.ts
@@ -0,0 +1,33 @@
+console.log("🤗 Hello via Bun! 🐰");
+const server = Bun.serve({
+    port: 8080,
+    fetch(req, server) {
+        const url = new URL(req.url);
+        if (url.pathname === "/") return new Response(Bun.file("./index.html"));
+        if (url.pathname === "/surprise") return new Response("🎁");
+        if (url.pathname === "/chat") {
+            if (server.upgrade(req)) {
+                return; // do not return a Response
+            }
+            return new Response("Filed upgrading to WebSocket", {status: 400});
+        }
+        return new Response("404!");
+    },
+    websocket: {
+        open(ws) {
+            console.log("👋 A new Websocket Connection");
+            ws.send("👋 Welcome baby");
+        },
+        message(ws, message) {
+            console.log("✉️ A new Websocket Message is received: " + message);
+            ws.send("✉️ Server received a message from you:  " + message);
+        },
+        close(ws, code, message) {
+            console.log("⏹️ A Websocket Connection is CLOSED");
+        },
+        drain(ws) {
+            console.log("DRAIN EVENT");
+        }, // the socket is ready to receive more data
+    }
+});
+console.log(`🚀 Server (HTTP and WebSocket) is launched ${server.url.origin}`);
